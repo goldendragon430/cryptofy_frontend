06cd7005f719a5c556ad0f81dbcffcbd7634d06e
FrankDevStar <reginafrankjames30@gmail.com>
2023-06-04 00:44:59 +0300

    Admin page

--- src/components/Management.tsx
+++ src/components/Management.tsx
@@ -1,6 +1,58 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import { useAuth } from "../contexts/SessionContext";
+import { useApi } from "../contexts/ApiContext";
+import {toast} from 'react-toastify'
 const Management: React.FC = () => {
+  const [{doPost}] = useApi();
+  const [user,] = useAuth()
+  const token = user?.token
+  const [mind,setMind] = useState(5)
+  const [minr,setMinr] = useState(7.5)
+  const [minw,setMinw] = useState(72)
+  const [lev1,setLev1] = useState(1)
+  const [lev2,setLev2] = useState(1)
+  const [lev3,setLev3] = useState(1)
+
+  useEffect(()=>{
+    if(token){
+      get_config()
+    }
+  },[token])
+
+  const get_config = async()=>{
+    const result = await doPost('mining/get_configuration',{
+      'token' : token
+    })
+    if(result.error||result['result'] == "failed"){
+      toast.error("Error")
+    }else{
+      const data = result['data']
+      setMind(data['bonus_rate'])
+      setMinr(data['min_reinvest'])
+      setMinw(data['min_withdrawl'])
+      setLev1(data['level_1'] * 100)
+      setLev2(data['level_2']* 100)
+      setLev3(data['level_3']* 100)
+    }
+  }
+
+  const update_config = async()=>{
+    const result = await doPost('mining/update_configuration',{
+      'token' : token,
+      'bonus_rate' : mind,
+      'min_w' : minw,
+      'min_r' : minr,
+      'lev_1' : lev1/100,
+      'lev_2' : lev2/100,
+      'lev_3' : lev3/100,
+    })
+    if(result.error||result['result'] == "failed"){
+      toast.error("Error")
+    }else{
+      toast.success("Success")
+    }
+  }
+
   return (
     <div className="flex h-full w-[75%] flex-col items-center justify-center gap-8 py-[2rem] pt-[8rem]">
       <div className="flex w-full flex-col gap-4 lg:grid lg:grid-cols-2">
@@ -12,26 +64,34 @@ const Management: React.FC = () => {
             <div className=" px-3 py-5">
               {/* <h1 className="mb-4 text-2xl font-medium text-white">Plan 1</h1> */}
 
-              <p className="mb-1 text-white">Min deposit amount</p>
+              <p className="mb-1 text-white">Bonus rate</p>
               <input
                 type="text"
-                className="mb-4 w-[95%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none focus:outline-4 focus:outline-slate-500"
+                value = {mind}
+                onChange = {e=>setMind(e.target.value)}
+                className="mb-4 w-[95%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none text-white"
               />
               <p className="mb-1 text-white">Min withdrawal amount</p>
               <input
                 type="text"
-                className="mb-4 w-[95%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none focus:outline-4 focus:outline-slate-500"
+                value = {minw}
+                onChange = {e=>setMinw(e.target.value)}
+                className="mb-4 w-[95%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none text-white"
               />
 
               <p className="mb-1 text-white">Min investment amount</p>
               <input
                 type="text"
-                className="w-[95%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none focus:outline-4 focus:outline-slate-500"
+                value = {minr}
+                onChange = {e=>setMinr(e.target.value)}
+                className="w-[95%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none text-white"
               />
             </div>
 
             <div className="mt-4 flex w-full justify-end border-t border-t-gray-200 p-2 px-10">
-              <button className="bg-gradient-moonlit rounded-md px-3 py-[0.4rem] font-medium text-white">
+              <button 
+              onClick = {update_config}
+              className="bg-gradient-moonlit rounded-md px-3 py-[0.4rem] font-medium text-white">
                 update
               </button>
             </div>
@@ -48,23 +108,31 @@ const Management: React.FC = () => {
               <p className="mb-1 text-white">Level 1 %</p>
               <input
                 type="text"
-                className="mb-4 w-[95%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none focus:outline-4 focus:outline-slate-500"
+                value = {lev1}
+                onChange = {e=>setLev1(e.target.value)}
+                className="mb-4 w-[95%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none text-white"
               />
               <p className="mb-1 text-white">Level 2 %</p>
               <input
                 type="text"
-                className="mb-4 w-[95%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none focus:outline-4 focus:outline-slate-500"
+                value = {lev2}
+                onChange = {e=>setLev2(e.target.value)}
+                className="mb-4 w-[95%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none text-white"
               />
 
               <p className="mb-1 text-white">Level 3 %</p>
               <input
                 type="text"
-                className="w-[95%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none focus:outline-4 focus:outline-slate-500"
+                value = {lev3}
+                onChange = {e=>setLev3(e.target.value)}
+                className="w-[95%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none text-white"
               />
             </div>
 
             <div className="mt-4 flex w-full justify-end border-t border-t-gray-200 p-2 px-10">
-              <button className="bg-gradient-moonlit rounded-md px-3 py-[0.4rem] font-medium text-white">
+              <button 
+               onClick={update_config}
+              className="bg-gradient-moonlit rounded-md px-3 py-[0.4rem] font-medium text-white">
                 update
               </button>
             </div>
@@ -74,20 +142,29 @@ const Management: React.FC = () => {
       <div className="flex flex-col lg:grid w-full lg:grid-cols-3 gap-2">
         <div className="flex w-full flex-col justify-start rounded-lg bg-cblack shadow-md">
           <div className="flex flex-col justify-start gap-6 px-3 py-5 pb-20">
-            <h1 className="mb-4 w-full text-center text-2xl font-medium text-white">
+            <h1 className=" w-full text-center text-2xl font-medium text-white">
               Plan 1
             </h1>
-            <div className="flex lg:w-[70%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
+            <div className=" lg:w-[100%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
               <p className="font-medium text-white">Amount:</p>
-              <p className="text-white">100</p>
+              <input
+                type="text"
+                className=" w-[90%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none text-white"
+              />
             </div>
-            <div className="flex lg:w-[70%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
+            <div className=" lg:w-[100%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
               <p className="font-medium text-white">Period:</p>
-              <p className="text-white">1 day</p>
+              <input
+                type="text"
+                className=" w-[90%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none text-white"
+              />
             </div>
-            <div className="flex lg:w-[70%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
+            <div className=" lg:w-[100%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
               <p className="font-medium text-white">Bonus rate:</p>
-              <p className="text-white">10%</p>
+              <input
+                type="text"
+                className=" w-[90%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none text-white"
+              />
             </div>
           </div>
 
@@ -95,41 +172,59 @@ const Management: React.FC = () => {
         </div>
         <div className="flex w-full flex-col justify-start rounded-lg bg-cblack shadow-md">
           <div className="flex flex-col justify-start gap-6 px-3 py-5 pb-20">
-            <h1 className="mb-4 w-full text-center text-2xl font-medium text-white">
+            <h1 className=" w-full text-center text-2xl font-medium text-white">
               Plan 2
             </h1>
-            <div className="flex lg:w-[70%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
+            <div className=" lg:w-[100%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
               <p className="font-medium text-white">Amount:</p>
-              <p className="text-white">100</p>
+              <input
+                type="text"
+                className=" w-[90%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none text-white"
+              />
             </div>
-            <div className="flex lg:w-[70%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
+            <div className=" lg:w-[100%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
               <p className="font-medium text-white">Period:</p>
-              <p className="text-white">1 day</p>
+              <input
+                type="text"
+                className=" w-[90%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none text-white"
+              />
             </div>
-            <div className="flex lg:w-[70%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
+            <div className=" lg:w-[100%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
               <p className="font-medium text-white">Bonus rate:</p>
-              <p className="text-white">10%</p>
+              <input
+                type="text"
+                className=" w-[90%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none text-white"
+              />
             </div>
           </div>
 
-         
+          
         </div>
         <div className="flex w-full flex-col justify-start rounded-lg bg-cblack shadow-md">
           <div className="flex flex-col justify-start gap-6 px-3 py-5 pb-20">
-            <h1 className="mb-4 w-full text-center text-2xl font-medium text-white">
+            <h1 className=" w-full text-center text-2xl font-medium text-white">
               Plan 3
             </h1>
-            <div className="flex lg:w-[70%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
+            <div className=" lg:w-[100%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
               <p className="font-medium text-white">Amount:</p>
-              <p className="text-white">100</p>
+              <input
+                type="text"
+                className=" w-[90%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none text-white"
+              />
             </div>
-            <div className="flex lg:w-[70%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
+            <div className=" lg:w-[100%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
               <p className="font-medium text-white">Period:</p>
-              <p className="text-white">1 day</p>
+              <input
+                type="text"
+                className=" w-[90%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none text-white"
+              />
             </div>
-            <div className="flex lg:w-[70%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
+            <div className=" lg:w-[100%] w-full items-center justify-between gap-20 self-start pl-1 lg:pl-5 text-lg">
               <p className="font-medium text-white">Bonus rate:</p>
-              <p className="text-white">10%</p>
+              <input
+                type="text"
+                className=" w-[90%] rounded-md border-none bg-slate-500 bg-opacity-40 p-2 text-white outline-none text-white"
+              />
             </div>
           </div>
 
